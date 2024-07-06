f4206f43d9f4a7e0067a0c2222e372f738abb696
nlpming <qqchenzhiming@163.com>
2024-07-06 22:11:01 +0800

    新增docker_cli_demo.sh

--- examples/demo/docker_cli_demo.sh
+++ examples/demo/docker_cli_demo.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+CONTAINER_NAME=qwen2
 
 #sudo docker run -d --gpus all -v ~/workspace:/home -p 8060:8888 nlpming/qwen:v1.0 \
 #  bash -c "while true; do sleep 1000; done"
 
-sudo docker run -d --gpus all -v ~/workspace:/home -p 8060:8888 nlpming/qwen:v1.0 \
-  python cli_demo.py -c /home/nlpming/workspace/Qwen2/models/Qwen2-0.5B-Instruct
+sudo docker run -it --gpus all --rm --name ${CONTAINER_NAME} -v ~/workspace:/home -p 8060:8888 \
+  nlpming/qwen:v1.0 python cli_demo.py -c /home/nlpming/workspace/Qwen2/models/Qwen2-0.5B-Instruct
 
 
